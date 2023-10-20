ff8eb1331621ea6470f2498def5c91bf39606e16
JadsonGomes <dev.gomes31@gmail.com>
2023-10-20 11:16:22 -0300

    testes de push

---
+++ main/variaveis/exec_variaveis.py
@@ -0,0 +1,12 @@
+#Uma variável é um objeto, afinal tudo é um objeto em Python
+
+#int -> inteiro
+#string -> texto
+#float -> números com casas decimais (ponto flutuante)
+#bool ou boolean -> True ou False 
+
+#Obs: Variáveis em Python não são declaradas explicitamente, mas isso não significa que você deve ficar mudando o tempo todo
+
+#Obs2: Cuidado com os nomes restritos (arquivo para Download)
+
+print('Olá, mundo')
